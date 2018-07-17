9e30d2fd247d405ab703e7552d4d73c871c7aa26
DESKTOP-BEAR877\sfsfk <sfsfkj@gmail.com>
2018-07-17 11:38:46 +0900

    install express and make html files

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-console.log('gg');
-console.log("hhh");
-console.log("qqq");
+var express = require('express');
+var http = require('http');
+var fs = require('fs')
+
+var app = express();
+app.set('port', process.env.PORT || 8081);
+
+var router = express.Router();
+router.route('/').get(function(req, res) {
+  res.sendfile(__dirname + '/main.html');
+});
+router.route('/page1').get(function(req, res) {
+  res.sendfile(__dirname + '/page1.html');
+});
+router.route('/page3').get(function(req, res) {
+  res.sendfile(__dirname + '/page1.html');
+});
+router.route('/page2').get(function(req, res) {
+  res.sendfile(__dirname + '/page1.html');
+});
+
+app.use('/',router);
+
+var server = http.createServer(app).listen(app.get('port'), function() {
+  console.log('initiate express server!');
+});
