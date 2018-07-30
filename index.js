1dbf85cd4d7f2101ceaf9be6d01368b346a8a615
DESKTOP-BEAR877\sfsfk <sfsfkj@gmail.com>
2018-07-30 18:28:55 +0900

    fix node

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var http = require('http');
+var https = require('https');
 var fs = require('fs')
+var bodyParser = require('body-parser');
+var url = require('url');
 
+var options = {
+  Key: fs.readFileSync('./Key.pem'),
+  cert: fs.readFileSync('./cert.pem')
+};
 var app = express();
 app.set('port', process.env.PORT || 8081);
+//app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 
 var router = express.Router();
 router.route('/').get(function(req, res) {
@@ -12,11 +22,20 @@ router.route('/').get(function(req, res) {
 router.route('/page1').get(function(req, res) {
   res.sendfile(__dirname + '/page1.html');
 });
-router.route('/page3').get(function(req, res) {
-  res.sendfile(__dirname + '/page1.html');
-});
 router.route('/page2').get(function(req, res) {
-  res.sendfile(__dirname + '/page1.html');
+  res.sendfile(__dirname + '/page2.html');
+});
+router.route('/page3').get(function(req, res) {
+  res.json({ text : 'sample text'});
+});
+router.route('/text').post(function(req, res) {
+  var text = req.body.text;
+  console.log("text : "+text);
+  var sendText = {
+    returnText : "send "+text
+  }
+  console.log("sned text : "+sendText.returnText);
+  res.json(sendText);
 });
 
 app.use('/',router);
